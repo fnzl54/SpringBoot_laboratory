84651dd827129e0b44e468b64ed20a845cfca74e
fnzl54 <rnjscksdud1@naver.com>
2023-04-30 23:21:08 +0900

    feat: 초기 셋팅

---
+++ src/test/java/SpringBoot_laboratory/chan/ChanApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot_laboratory.chan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
