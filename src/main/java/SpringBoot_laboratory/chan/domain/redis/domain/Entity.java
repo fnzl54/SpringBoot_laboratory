933dca0f7e6cdbe38238dcd7af69bbaee70e3517
fnzl54 <rnjscksdud1@naver.com>
2023-05-05 16:52:08 +0900

    feat: redis entity 추가

---
+++ src/main/java/SpringBoot_laboratory/chan/domain/redis/domain/Entity.java
@@ -0,0 +1,24 @@
+package SpringBoot_laboratory.chan.domain.redis.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RedisHash(value = "people", timeToLive = 120)
+public class Entity {
+
+    @Id
+    private String id;
+    private String name;
+    private Integer age;
+    private LocalDateTime createdAt;
+
+    public Entity(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+}
