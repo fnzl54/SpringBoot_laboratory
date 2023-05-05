9c2a1b3edd6d6a55319b0da2b972b96c05bca381
fnzl54 <rnjscksdud1@naver.com>
2023-05-05 22:51:38 +0900

    feat: redistemplete test 추가

--- src/test/java/SpringBoot_laboratory/chan/ChanApplicationTests.java
+++ src/test/java/SpringBoot_laboratory/chan/ChanApplicationTests.java
@@ -5,15 +5,20 @@ import SpringBoot_laboratory.chan.domain.redis.domain.RedisRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
+
+import java.util.*;
 
 @SpringBootTest
 class ChanApplicationTests {
 
 	@Autowired
 	private RedisRepository repository;
+	@Autowired
+	private StringRedisTemplate redisTemplate;
 
 	@Test
-	void test() {
+	void redis레포지토리테스트() {
 		Entity person = new Entity("Park", 20);
 
 		// 저장
@@ -28,5 +33,48 @@ class ChanApplicationTests {
 		// 삭제
 //		repository.delete(person);
 	}
+
+	@Test
+	public void RedisTemplate테스트_String() {
+		final String key = "sabarada";
+
+		final ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
+
+		stringStringValueOperations.set(key, "1"); // redis set 명령어
+		final String result_1 = stringStringValueOperations.get(key); // redis get 명령어
+
+		System.out.println("result_1 = " + result_1);
+
+		stringStringValueOperations.increment(key); // redis incr 명령어
+		final String result_2 = stringStringValueOperations.get(key);
+
+		System.out.println("result_2 = " + result_2);
+	}
+
+	public void RedisTemplate테스트_Set() {
+		final String key = "sabarada";
+		final SetOperations<String, String> stringStringSetOperations = redisTemplate.opsForSet();
+
+		stringStringSetOperations.add(key, "H");
+		stringStringSetOperations.add(key, "e");
+		stringStringSetOperations.add(key, "l");
+		stringStringSetOperations.add(key, "l");
+		stringStringSetOperations.add(key, "o");
+
+		Set<String> sabarada = stringStringSetOperations.members(key);
+
+		System.out.println("members = " + Arrays.toString(sabarada.toArray()));
+
+		Long size = stringStringSetOperations.size(key);
+
+		System.out.println("size = " + size);
+
+		Cursor<String> cursor = stringStringSetOperations.scan(key, ScanOptions.scanOptions().match("*").count(3).build());
+
+		while(cursor.hasNext()) {
+			System.out.println("cursor = " + cursor.next());
+		}
+	}
+
 
 }
